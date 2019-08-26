8a85979330fdb6a3d4512f8e33fdea3af0c60066
malikmsaeed <malikmsaeed88@gmail.com>
2019-08-26 11:09:42 +0500

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "../src/pages/Home";
+import Recipes from "../src/pages/Recipes";
+import SingleRecipe from "../src/pages/SingleRecipe";
+import Default from "../src/pages/Default";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div>Hello from out recipe app</div>;
+  return (
+    <Router>
+      <main>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/recipes" exact component={Recipes} />
+          <Route path="/recipes/:id" component={SingleRecipe} />
+          <Route component={Default} />
+        </Switch>
+      </main>
+    </Router>
+  );
 }
 
 export default App;
