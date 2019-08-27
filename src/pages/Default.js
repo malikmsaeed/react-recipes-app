dc76435d8c51c632ae80b8b0fd4d4fb9c5aa7e51
malikmsaeed <malikmsaeed88@gmail.com>
2019-08-27 11:28:18 +0500

    Adding Default Page Content

--- src/pages/Default.js
+++ src/pages/Default.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
-
+import Header from "../components/Header";
+import { Link } from "react-router-dom";
 export default class Default extends Component {
   render() {
-    return <h4>Hello From Error Page</h4>;
+    return (
+      <Header title="404" styleClass="default-hero">
+        <h2 className="text-uppercase text-light">You are at wrong Place</h2>
+        <Link to="/" className="btn btn-secondary text-uppercase mt-3 btn-lg">
+          return Home
+        </Link>
+      </Header>
+    );
   }
 }
